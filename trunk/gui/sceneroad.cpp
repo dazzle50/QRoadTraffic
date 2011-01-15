cdf791f8c42e103ad619bf375696fda67c34f2f2
dazzle50 <dazzle50@084f39ee-d868-11de-8231-e962dcc7f716>
2011-01-15 12:44:37 +0000

    Updated copyright year to 2011 & updated design docs

--- trunk/gui/sceneroad.cpp
+++ trunk/gui/sceneroad.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2010 by Richard Crook                                   *
+ *   Copyright (C) 2011 by Richard Crook                                   *
  *   http://code.google.com/p/qroadtraffic/                                *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
